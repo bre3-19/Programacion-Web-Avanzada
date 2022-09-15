5d7acc563b86a6d269efbf145f9ccee859b852ae
bre3-19 <95944174+bre3-19@users.noreply.github.com>
2022-09-15 09:54:24 -0600

    Add files via upload

---
+++ 15-09-2022/index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title> Vue js </title>
+        <link rel="stylesheet" href="public/css/main.css">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    </head>
+
+    <body>
+        <div class = "container">
+            <form>
+                <fieldset>
+                    <legend>
+                        Datos de inicio
+                    </legend>
+                    <label> 
+                        E-mail: 
+                    </label>
+                    <input type="text" name="">
+                    
+                    <label> 
+                        Password: 
+                    </label>
+                    <input type="password" name="">
+
+                    <button type="submit">Log In</button>
+
+                </fieldset>
+            </form>
+        </div>
+    </body>
+</html>
