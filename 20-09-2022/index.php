34b2795de35ee77291877dcc7ed3f5cbca7be49a
bre3-19 <95944174+bre3-19@users.noreply.github.com>
2022-09-20 10:02:11 -0600

    Add files via upload

---
+++ 20-09-2022/index.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <title> Vue js </title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <!--<link rel="stylesheet" href="public/css/main.css?v=1">-->
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    </head>
+
+    <body>
+      <div class="container">
+        <div class="row-align-items-center">
+          <div class="col-md-3">
+            Hola
+          </div>
+          <div class="col-6">
+            <form>
+              <fieldset>
+              <legend>
+                Datos de inicio
+              </legend>
+                <div class="form-outline mb-4">
+                  <label> 
+                    E-mail: 
+                  </label>
+                  <input type="text" name="">
+                </div>
+
+                <div class="form-outline mb-4">  
+                  <label> 
+                    Password: 
+                  </label>
+                  <input type="password" name="">
+                </div>
+                <button type="submit" class="btn btn-primary btn-block mb-4">Log In</button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+
+    </body>
+</html>
